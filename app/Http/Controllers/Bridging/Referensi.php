bef584977fa716dbc053573a85b00f00df82a7fa
Fazri Anggriawan <fazri.anggriawan@gmail.com>
2022-07-20 23:21:34 +0700

    get jadwal dokter hari ini

--- app/Http/Controllers/Bridging/Referensi.php
+++ app/Http/Controllers/Bridging/Referensi.php
@@ -10,95 +10,97 @@ use Illuminate\Http\Client\Request;
 class Referensi extends Controller
 {
     public function Diagnosa($keyword)
-	{
-		$url = 'referensi/diagnosa/'.$keyword;
+    {
+        $url = 'referensi/diagnosa/' . $keyword;
         return VclaimLib::exec('GET', $url);
-	}
+    }
 
     public function Poliklinik($keyword)
-	{
-        return VclaimLib::exec('GET', 'referensi/poli/'.$keyword);
-	}
+    {
+        return VclaimLib::exec('GET', 'referensi/poli/' . $keyword);
+    }
 
     public function Faskes($keyword)
-	{
-		$url = 'referensi/faskes/'.$keyword.'/2';
+    {
+        $url = 'referensi/faskes/' . $keyword . '/2';
         return VclaimLib::exec('GET', $url);
-	}
+    }
 
     public function Procedure(Request $request)
-	{
-        $url = 'referensi/procedure/'.$request->input('key');
+    {
+        $url = 'referensi/procedure/' . $request->input('key');
         return VclaimLib::exec('GET', $url);
-	}
+    }
 
     public function KelasRawat()
-	{
+    {
         return VclaimLib::exec('GET', 'referensi/kelasrawat');
-	}
+    }
 
     public function Dokter(Request $request)
-	{
-        $url = 'referensi/dokter/'.$request->input('key');
+    {
+        $url = 'referensi/dokter/' . $request->input('key');
         return VclaimLib::exec('GET', $url);
-	}
+    }
 
     public function Spesialistik()
-	{
-		return VclaimLib::exec('GET', 'referensi/spesialistik');
-	}
+    {
+        return VclaimLib::exec('GET', 'referensi/spesialistik');
+    }
 
     public function RuangRawat()
-	{
+    {
         return VclaimLib::exec('GET', 'referensi/ruangrawat');
-	}
+    }
 
     public function CaraKeluar()
-	{
+    {
         return VclaimLib::exec('GET', 'referensi/carakeluar');
-	}
+    }
 
     public function PascaPulang()
-	{
+    {
         return VclaimLib::exec('GET', 'referensi/pascapulang');
-	}
+    }
 
     public function Kabupaten(Request $request)
-	{
-        $url = 'referensi/kabupaten/propinsi/'.$request->input('id_propinsi');
+    {
+        $url = 'referensi/kabupaten/propinsi/' . $request->input('id_propinsi');
         return VclaimLib::exec('GET', $url);
-	}
+    }
 
     public function Kecamatan(Request $request)
-	{
-		$url = 'referensi/kecamatan/kabupaten/'.$request->input('id_kabupaten');
-		return VclaimLib::exec('GET', $url);
-	}
+    {
+        $url = 'referensi/kecamatan/kabupaten/' . $request->input('id_kabupaten');
+        return VclaimLib::exec('GET', $url);
+    }
 
-	public function ProgramPrb()
-	{
-		return VclaimLib::exec('GET', 'referensi/diagnosaprb');
-	}
+    public function ProgramPrb()
+    {
+        return VclaimLib::exec('GET', 'referensi/diagnosaprb');
+    }
 
-	public function DokterDpjp($jnsPelayanan, $poliklinik)
-	{
-		$tgl_pelayanan = Date('Y-m-d');
+    public function DokterDpjp($jnsPelayanan, $poliklinik)
+    {
+        $tgl_pelayanan = Date('Y-m-d');
 
-		$url = 'referensi/dokter/pelayanan/'.$jnsPelayanan.'/tglPelayanan/'.$tgl_pelayanan.'/Spesialis/'.$poliklinik;
+        $url = 'referensi/dokter/pelayanan/' . $jnsPelayanan . '/tglPelayanan/' . $tgl_pelayanan . '/Spesialis/' . $poliklinik;
 
-		return VclaimLib::exec('GET', $url);
-	}
+        return VclaimLib::exec('GET', $url);
+    }
 
-	public function ObatPrb(Request $request)
-	{
-		$url = 'referensi/obatprb/'.$request->input('key');
-		return VclaimLib::exec('GET', $url);
-	}
+    public function ObatPrb(Request $request)
+    {
+        $url = 'referensi/obatprb/' . $request->input('key');
+        return VclaimLib::exec('GET', $url);
+    }
 
-	public function JadwalDokter($kodePoli, $tglKunjungan)
-	{
-		$url = 'jadwaldokter/kodepoli/'.$kodePoli.'/tanggal/'.$tglKunjungan;
-		return AntrolLib::exec('GET', $url);
-	}
-
+    public function JadwalDokter($kodePoli, $tglKunjungan)
+    {
+        if ($tglKunjungan == 'now') {
+            $tglKunjungan =  date('Y-m-d');
+        }
+        $url = 'jadwaldokter/kodepoli/' . $kodePoli . '/tanggal/' . $tglKunjungan;
+        return AntrolLib::exec('GET', $url);
+    }
 }
