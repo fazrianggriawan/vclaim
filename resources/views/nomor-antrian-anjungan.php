b4a00d046a398c1eff0febc92ecc2f2f48089134
Fazri Anggriawan <fazri.anggriawan@gmail.com>
2022-08-25 11:31:17 +0700

    tambahin tag php di template

--- resources/views/nomor-antrian-anjungan.php
+++ resources/views/nomor-antrian-anjungan.php
@@ -13,22 +13,22 @@
 <div class="container">
     <div style="font-size: 14px; font-weight: bold; margin-bottom: 5px;">
         <div>RUMAH SAKIT SALAK - BOGOR</div>
-        <? if( $tipe == 'tni' ){ ?><div>TNI / KELUARGA TNI / PURNAWIRAWAN</div> <? } ?>
-        <? if( $tipe == 'bpjs' ){ ?><div>BPJS KESEHATAN</div> <? } ?>
-        <? if( $tipe == 'tunai' ){ ?><div>PASIEN UMUM</div> <? } ?>
+        <?php if( $tipe == 'tni' ){ ?><div>TNI / KELUARGA TNI / PURNAWIRAWAN</div> <?php } ?>
+        <?php if( $tipe == 'bpjs' ){ ?><div>BPJS KESEHATAN</div> <?php } ?>
+        <?php if( $tipe == 'tunai' ){ ?><div>PASIEN UMUM</div> <?php } ?>
     </div>
     <div style="font-size: 16px; font-weight: bold; margin-bottom: 10px; margin-top: 10px;">
         <div>NOMOR ANTRIAN</div>
     </div>
 
     <div style="font-size: 32px; font-weight: bold; margin-bottom: 20px;">
-        <div><? echo $noAntrian ?></div>
+        <div><?php echo $noAntrian ?></div>
     </div>
 
     <div style="font-size: 12px;">
-        <? if( $tipe == 'tunai' ){ ?> <div>Silahkan Menunggu di Loket 1 atau 2</div> <? } ?>
-        <? if( $tipe == 'bpjs' ){ ?> <div>Silahkan Menunggu di Loket 3 atau 4</div> <? } ?>
-        <? if( $tipe == 'tni' ){ ?> <div>Silahkan Menunggu di Loket 5 atau 6</div> <? } ?>
+        <?php if( $tipe == 'tunai' ){ ?> <div>Silahkan Menunggu di Loket 1 atau 2</div> <?php } ?>
+        <?php if( $tipe == 'bpjs' ){ ?> <div>Silahkan Menunggu di Loket 3 atau 4</div> <?php } ?>
+        <?php if( $tipe == 'tni' ){ ?> <div>Silahkan Menunggu di Loket 5 atau 6</div> <?php } ?>
 
         <div>Tgl. Cetak : <? echo date('d-m-Y')?> Jam : <? echo date('H:i:s') ?></div>
     </div>
