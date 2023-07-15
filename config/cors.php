ffc6bd5245ef4ec1120152e42071bffd0a45c098
Simrs Mandiri Client Salak <fazri.anggriawan@gmail.com>
2023-07-15 16:07:53 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,28 +1,66 @@
 <?php
-    date_default_timezone_set("Asia/Jakarta");
 
-    // header('Access-Control-Allow-Origin: http://localhost:4200');
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        // header('Access-Control-Allow-Origin: http://localhost:4200');
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
+return [
 
-    // Access-Control headers are received during OPTIONS requests
-    if( isset($_SERVER['REQUEST_METHOD']) ){
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS Options
+    |--------------------------------------------------------------------------
+    |
+    | The allowed_methods and allowed_headers options are case-insensitive.
+    |
+    | You don't need to provide both allowed_origins and allowed_origins_patterns.
+    | If one of the strings passed matches, it is considered a valid origin.
+    |
+    | If ['*'] is provided to allowed_methods, allowed_origins or allowed_headers
+    | all methods / origins / headers are allowed.
+    |
+    */
 
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    /*
+     * You can enable CORS for 1 or multiple paths.
+     * Example: ['api/*']
+     */
+    'paths' => ['*'],
 
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    /*
+    * Matches the request method. `['*']` allows all methods.
+    */
+    'allowed_methods' => ['*'],
 
-            exit(0);
-        }
-    }
+    /*
+     * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
+     */
+    'allowed_origins' => [
+        'http://localhost',
+        'http://localhost:4200',
+        'http://192.168.107.3',
+        'http://192.168.107.4',
+        'http://rikkes.rssalakbogor.co.id',
+        'http://online.rssalakbogor.co.id'],
 
+    /*
+     * Patterns that can be used with `preg_match` to match the origin.
+     */
+    'allowed_origins_patterns' => ['*'],
 
+    /*
+     * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
+     */
+    'allowed_headers' => ['*'],
 
+    /*
+     * Sets the Access-Control-Expose-Headers response header with these headers.
+     */
+    'exposed_headers' => ['*'],
+
+    /*
+     * Sets the Access-Control-Max-Age response header when > 0.
+     */
+    'max_age' => 0,
+
+    /*
+     * Sets the Access-Control-Allow-Credentials header.
+     */
+    'supports_credentials' => false,
+];
